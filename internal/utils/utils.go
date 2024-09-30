1f9a0cdd8aa4a415da1fcf5160f812e5a3843fd3
Andrew Domingues <contato@drewdomi.com>
2024-09-30 08:10:32 -0300

    chore: add godotenv

---
+++ internal/utils/utils.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GetEnvVar(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return os.Getenv(key)
+}
